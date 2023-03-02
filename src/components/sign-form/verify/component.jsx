97255d390f95fc5f3fdea4115333ebe72402a4d8
double-hope <nadia.prohorchuk@gmail.com>
2023-03-02 23:58:37 +0200

    add verify page

---
+++ src/components/sign-form/verify/component.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const notify = () => toast.success('Confirm your e-mail and sign in!', {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    });
+
+const Verify = () => {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        notify();
+        setTimeout(() => {
+            navigate('/sign-in');
+        }, 2000);
+    }, [navigate]);
+
+  return (
+    <div>
+        <ToastContainer />
+        Verify your  email!
+        Don't get a letter?
+        Resend 
+    </div>
+  )
+}
+
+export { Verify };
