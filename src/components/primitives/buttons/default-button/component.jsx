d8d8e2ffc87fa938bd21376fd94f41eae98d3128
double-hope <nadia.prohorchuk@gmail.com>
2023-03-03 16:12:01 +0200

    style button component

--- src/components/primitives/buttons/default-button/component.jsx
+++ src/components/primitives/buttons/default-button/component.jsx
@@ -1,11 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as styles from './styles';
+import { faCircleDot } from '@fortawesome/free-solid-svg-icons';
+import { ButtonColorEnum, ButtonShapeEnum, ButtonSizeEnum } from 'common/enums';
 
-const Button = ({text, onClick, ...props}) => {
+const Button = ({text, onClick, color, size, shape, ...props}) => {
   return (
-    <button css={styles.button} onClick={onClick} {...props}>
+    <button css={styles.button} onClick={onClick} button-color={color} button-size={size} button-shape={shape} {...props}>
+      <FontAwesomeIcon icon={faCircleDot} />
       {text}
+      <FontAwesomeIcon icon={faCircleDot} />
     </button>
   )
+}
+
+Button.defaultProps = {
+  size: ButtonSizeEnum.DEFAULT,
+  color: ButtonColorEnum.DARK,
+  shape: ButtonShapeEnum.RECTANGLE,
 }
 
 export { Button };
--- src/components/primitives/buttons/default-button/styles.js
+++ src/components/primitives/buttons/default-button/styles.js
@@ -1,20 +1,16 @@
 import { css } from '@emotion/react';
+import { ButtonColorEnum, ButtonShapeEnum, ButtonSizeEnum } from 'common/enums';
 
 export const button = () => css`
     display: flex;
-    justify-content: center;
+    justify-content: space-around;
     align-items: center;
+    gap: 10px;
     width: 100%;
-    max-width: 250px;
     margin: 10px 0;
-    padding: 10px;
-    background-color: #4267B2;
-    color: white;
-    border: none;
-    box-shadow: 0 0 5px hsla(0, 0%, 0%, 0.4);
-    border-radius: 20px;
-    font-weight: 600;
-    font-size: 1.05rem;
+    font-weight: 900;
+    font-size: 12px;
+    line-height: 20px;
     cursor: pointer;
 
     &:disabled {
@@ -22,4 +18,67 @@ export const button = () => css`
         color: #575859;
         cursor: not-allowed;
     }
+
+    &[button-color=${ButtonColorEnum.PRIMARY}] {
+        background-color: #3361FF;
+        border: 2px solid #3361FF;
+        color: white;
+
+        & svg {
+            color: #FFFFFF;
+        }
+    }
+
+    &[button-color=${ButtonColorEnum.LIGHT}] {
+        background-color: #FFFFFF;
+        box-shadow: 0px 2px 5px rgba(38, 51, 77, 0.03);
+        border: 2px solid #F5F6F7;
+        color: #6B7A99;
+
+        & svg {
+            color: #C3CAD9;
+        }
+    }
+
+    &[button-color=${ButtonColorEnum.DARK}] {
+        background: #202B40;
+        border: 2px solid #2D3C59;
+        color: #7D8FB3;
+
+        & svg {
+            color: #4D5E80;
+        }
+    }
+
+    &[button-size=${ButtonSizeEnum.LARGE}] {
+        border-radius: 30px;
+        padding: 15px 25px;
+        max-width: 173px;
+        font-size: 14px;
+        line-height: 30px;
+    }
+
+    &[button-size=${ButtonSizeEnum.DEFAULT}] {
+        border-radius: 30px;
+        padding: 10px 20px;
+        max-width: 157px;
+    }
+
+    &[button-size=${ButtonSizeEnum.SMALL}] {
+        border-radius: 20px;
+        padding: 6px 15px;
+        max-width: 133px;
+    }
+
+    &[button-size=${ButtonSizeEnum.EXTRA_SMALL}] {
+        border-radius: 20px;
+        padding: 3px 10px;
+        max-width: 109px;
+        font-size: 10px;
+    }
+
+    &[button-shape=${ButtonShapeEnum.RECTANGLE}] {
+        border-radius: 5px;
+    }
+
 `;
